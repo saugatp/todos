0bb864b31b6dfc319b0619c217915d357b328913
Saugat Poudel <poudelsaugat45@gmail.com>
2024-07-03 22:41:48 +0100

    Initial Commit

---
+++ todos/todosApp.swift
@@ -0,0 +1,17 @@
+//
+//  todosApp.swift
+//  todos
+//
+//  Created by Saugat Poudel on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct todosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ todosUITests/todosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  todosUITestsLaunchTests.swift
+//  todosUITests
+//
+//  Created by Saugat Poudel on 03/07/2024.
+//
+
+import XCTest
+
+final class todosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
